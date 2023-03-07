ab4539639ed2577751a8fe246e17db490787be6e
Marjan Stojanovski <macikb8@yahoo.com>
2023-03-07 21:51:31 +0100

    fix the active class on nav links

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,17 @@
+const navItems = document.querySelectorAll(".nav-item");
+
+document.addEventListener("DOMContentLoaded", function () {
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener("click", function () {
+      for (let j = 0; j < navItems.length; j++) {
+        navItems[j].classList.remove("active");
+
+        this.classList.add("active");
+      }
+    });
+  }
+});
+
 function calculateCal() {
   document.getElementById("form").reset();
 }
