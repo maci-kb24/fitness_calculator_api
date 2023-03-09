299c1afd99483f0199d549f8e8892e9cee98cdb1
Marjan Stojanovski <macikb8@yahoo.com>
2023-03-09 18:36:35 +0100

    fix style form calculator

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,4 @@
+// toggle active class on nav items
 const navItems = document.querySelectorAll(".nav-item");
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -12,6 +13,55 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-function calculateCal() {
-  document.getElementById("form").reset();
-}
+//system
+const systemImperial = document.getElementById("imperial").value;
+const systemMetric = document.getElementById("metric").value;
+
+// age range display
+const ageRange = document.getElementById("age-range");
+const ageDisplay = document.getElementById("age-display");
+
+ageRange.addEventListener("input", function () {
+  let ageValue = ageRange.value;
+  ageDisplay.textContent = `I am ${ageValue} years old`;
+});
+
+// height range display
+const heightRange = document.getElementById("height-range");
+const heightDisplay = document.getElementById("height-display");
+
+heightRange.addEventListener("input", function () {
+  let heightValue = heightRange.value;
+  heightDisplay.textContent = `My height: ${heightValue} cm`;
+});
+
+//weight range display
+const weightRange = document.getElementById("weight-range");
+const weightDisplay = document.getElementById("weight-display");
+
+weightRange.addEventListener("input", function () {
+  let weightValue = weightRange.value;
+  weightDisplay.textContent = `Current weight: ${weightValue} kg`;
+});
+
+//form submit
+
+let formCalc = document.getElementById("form");
+
+formCalc.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  ageValue = ageRange.value;
+  heightValue = heightRange.value;
+  weightValue = weightRange.value;
+
+  if (systemImperial === false) {
+    systemImperial;
+  } else {
+    systemMetric;
+  }
+
+  console.log(systemImperial, systemMetric);
+
+  formCalc.reset();
+});
